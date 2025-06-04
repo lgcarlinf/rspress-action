fda784102dedd07adc92d167b863a6fa7c24587e
luiggy <luiggylml@gmail.com>
2025-06-04 17:16:41 -0500

    chore: update base path

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname , 'docs'),
-  base:'rspress-action',
+  base:'/rspress-action/',
   title: 'My Site',
   icon: '/rspress-icon.png',
   logo: {
